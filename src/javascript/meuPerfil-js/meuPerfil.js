83430d7a04aa6e6499070829742dd519b07a041e
GUSParra <hpbenitez7@gmail.com>
2023-04-14 16:07:11 -0300

    att meu perfil

---
+++ src/javascript/meuPerfil-js/meuPerfil.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const btnEditar = document.querySelector('.edit-perfil');
+    btnEditar.addEventListener('click', function() {
+      alert('Botão de editar perfil clicado!');
+    });
+  });
+  
