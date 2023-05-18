818dba3b97c240fd29e9849f1d10110793bc46d4
Parra <hpbenitez7@gmail.com>
2023-05-18 04:02:31 -0300

    adicionando funcionalidade de mudar foto

--- src/javascript/meuPerfil-js/meuPerfil.js
+++ src/javascript/meuPerfil-js/meuPerfil.js
@@ -1,4 +1,20 @@
-const btnEditar = document.querySelector('.edit-perfil');
-btnEditar.addEventListener('click', function() {
-  alert('Botão de editar perfil clicado!');
+// Obtém referências aos elementos do HTML
+const fotoInput = document.getElementById('foto-input');
+const mudarFotoBtn = document.getElementById('mudar-foto');
+const avatarDiv = document.querySelector('.avatar');
+
+// Adiciona um ouvinte de evento para o botão de mudar foto
+mudarFotoBtn.addEventListener('click', () => {
+  fotoInput.click(); // Simula o clique no input de foto
+});
+
+// Ouve o evento de mudança no input de foto
+fotoInput.addEventListener('change', (event) => {
+  const file = event.target.files[0]; // Obtém o arquivo selecionado
+
+  // Cria um objeto URL para representar a imagem selecionada
+  const imageUrl = URL.createObjectURL(file);
+
+  // Atualiza a imagem do avatar com a nova foto
+  avatarDiv.style.backgroundImage = `url('${imageUrl}')`;
 });
